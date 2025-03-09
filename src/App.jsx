92f3f86af6c9fbd3c9d7e406b8ef7f527da1032f
Arpina1 <nazifeburcu.acikoz@stonybrook.edu>
2025-03-08 21:40:35 -0500

    changed tailwind dependency

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ReferenceForm from './componenets/ReferenceForm'
 import './App.css'
+import './test.css'
 
 function App() {
   const [count, setCount] = useState(0)
